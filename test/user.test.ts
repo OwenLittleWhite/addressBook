4e64dd46a5b5185c38fe78f09d10e651d46514cc
owen.zhao <owen.zhao@sz.questwork.com>
2017-10-24 10:15:39 +0800

    modify testcase

--- test/user.test.ts
+++ test/user.test.ts
@@ -1,8 +1,8 @@
 import { } from "jest";
 import * as supertest from "supertest";
 import app from "../src/server";
-// const request = supertest.agent(app);
-const request = supertest("http://127.0.0.1:3000");
+const request = supertest.agent(app);
+// const request = supertest("http://127.0.0.1:3000");
 describe("get /users", () => {
   it("should return 200 OK", async () => {
    await request.get("/users").expect(200).end()
